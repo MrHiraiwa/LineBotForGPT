d04585bed2066367f1a16e660be76babfb16c3a5
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-05-19 23:46:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,12 +36,19 @@ def get_encrypted_message(message, hashed_secret_key):
     return enc_message.decode()
 
 def get_decrypted_message(enc_message, hashed_secret_key):
-    cipher = AES.new(hashed_secret_key, AES.MODE_ECB)
-    enc_message = base64.b64decode(enc_message.encode('utf-8'))
-    message = cipher.decrypt(enc_message)
-    padding = message[-1]
-    message = message[:-padding]
-    return message.decode().rstrip("\0")
+    try:
+        cipher = AES.new(hashed_secret_key, AES.MODE_ECB)
+        enc_message = base64.b64decode(enc_message.encode('utf-8'))
+        message = cipher.decrypt(enc_message)
+        padding = message[-1]
+        if padding > 16:
+            raise ValueError("Invalid padding value")
+        message = message[:-padding]
+        return message.decode().rstrip("\0")
+    except Exception as e:
+        print(f"Error decrypting message: {e}")
+        return None
+
 
 
 def isBeforeYesterday(date, now):
