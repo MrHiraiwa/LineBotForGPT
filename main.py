d476e32a9636a09a91ec0f7e6d503929b9a534a1
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-05-25 14:26:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -291,7 +291,7 @@ def lineBot():
             elif message_type == 'location':
                 exec_functions = True
                 userMessage = "位置情報が送信されました。"
-            elif any(word in userMessage for word in "🌐インターネットで「")
+            elif "🌐インターネットで「" in userMessage:
                 exec_functions = True
                 userMessage = remove_specific_character(userMessage, '」を検索')
                 userMessage = remove_specific_character(userMessage, '🌐インターネットで「')
