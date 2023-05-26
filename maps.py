10e92dda053d4cd4503ad512f139ac854d79fecc
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-05-26 20:51:52 +0900

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,1 +1,21 @@
+import requests
+import os
+import json
 
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+def find_place_by_geo_info(latitude, longitude, keyword1):
+    places_api_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    parameters = {
+        'keyword': keyword1,
+        'types': 'food',
+        'language': 'ja',
+        'location': f'{latitude},{longitude}',
+        'radius': 1000,
+        'key': GOOGLE_API_KEY
+    }
+
+    response = requests.get(places_api_url, params=parameters)
+    data = response.json()
+
+    return data
