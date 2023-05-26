edabc8ba522866067f618cfe2b4cb3a9985e35ab
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-05-27 02:11:32 +0900

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -7,40 +7,6 @@ from io import BytesIO
 OPENAI_APIKEY = os.getenv('OPENAI_APIKEY')
 LINE_ACCESS_TOKEN = os.getenv('LINE_ACCESS_TOKEN')
 
-def speech_to_text(file):
-    payload = {
-        'model': 'whisper-1',
-        'temperature': 0
-    }
-
-    headers = {
-        'Authorization': f'Bearer {OPENAI_APIKEY}'
-    }
-    
-    files = {
-        'file': ('file', file)
-    }
-
-    try:
-        response = requests.post(
-            "https://api.openai.com/v1/audio/transcriptions", 
-            headers=headers, 
-            data=payload,
-            files=files
-        )
-        response.raise_for_status()  # Raises a HTTPError if the status is 4xx, 5xx
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP error occurred: {err}")
-        print(f"Headers: {headers}")
-        print(f"Payload: {payload}")
-        return
-
-    if response.status_code == 200:
-        return response.json().get('text')
-    else:
-        return response.content
-
-
 def get_audio(message_id):
     url = f'https://api-data.line.me/v2/bot/message/{message_id}/content'
 
@@ -51,8 +17,41 @@ def get_audio(message_id):
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        # Create a temporary file in memory
-        temp = BytesIO(response.content)
+        # Save the audio file temporarily
+        with NamedTemporaryFile(suffix=".m4a", delete=False) as temp:
+            temp.write(response.content)
+            temp.flush()
 
         # Call the speech_to_text function with the temporary file
-        return speech_to_text(temp)
+        return speech_to_text(temp.name)
+    else:
+        print(f"Failed to fetch audio: {response.content}")
+        return None
+
+def speech_to_text(file_path):
+    with open(file_path, 'rb') as f:
+        payload = {
+            'model': 'whisper-1',
+            'temperature': 0
+        }
+
+        headers = {
+            'Authorization': f'Bearer {OPENAI_APIKEY}'
+        }
+
+        files = {
+            'file': (os.path.basename(file_path), f)
+        }
+
+        response = requests.post(
+            "https://api.openai.com/v1/audio/transcriptions", 
+            headers=headers, 
+            data=payload, 
+            files=files
+        )
+
+        if response.status_code == 200:
+            return response.json().get('text')
+        else:
+            print(f"Failed to transcribe audio: {response.content}")
+            return None
