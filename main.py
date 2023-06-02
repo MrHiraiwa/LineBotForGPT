0f3fdb2d56f2c590f1bb8657aa940dfe9fa7a8df
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-02 20:49:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -536,7 +536,7 @@ def lineBot():
                 # After sending the audio, delete the local file
                 if success:
                     delete_local_file(local_path)
-            if not quick_reply_on == True:            
+            if quick_reply_on == False:            
                 if voice_or_text == "VOICE" and VOICE_ON == 'Reply':
                     blob_path = f'{userId}/{message_id}.m4a'
                     public_url, local_path, duration = text_to_speech(botReply, BACKET_NAME, blob_path)
