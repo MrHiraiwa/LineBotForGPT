1c6aa0a57d1aebc23d0cece937010acaa3e1f819
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-03 18:38:58 +0900

    Update voice.py

--- voice.py
+++ voice.py
@@ -32,11 +32,11 @@ def convert_audio_to_m4a(input_path, output_path):
     #print("stdout:", result.stdout)
     #print("stderr:", result.stderr)
 
-def text_to_speech(text, bucket_name, destination_blob_name):
+def text_to_speech(text, bucket_name, destination_blob_name,userMessage):
     client = texttospeech.TextToSpeechClient()
     synthesis_input = texttospeech.SynthesisInput(text=text)
     
-    detected_lang, dialect = detect_language(text)
+    detected_lang, dialect = detect_language(userMessage)
 
     if detected_lang == 'ja':
         language_code = "ja-JP"
