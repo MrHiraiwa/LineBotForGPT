321ab2d83b0f1a74ece32b2f860f22f0badc9968
M.HIRAIWA <133576654+MrHiraiwa@users.noreply.github.com>
2023-06-06 13:10:17 +0900

    Update payment.py

--- payment.py
+++ payment.py
@@ -4,7 +4,7 @@ import stripe
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
 def create_checkout_session(amount, currency, success_url, cancel_url):
-    stripe.api_key = 'STRIPE_SECRET_KEY'
+    stripe.api_key = STRIPE_SECRET_KEY
 
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
